6ea49bfecaa1e275c9dbf1083f737c3deb560dba
jcocmpo <jcocampojc15@gmail.com>
2025-10-19 22:23:46 +0800

    binary design

--- src/app/games/binarygame/page.js
+++ src/app/games/binarygame/page.js
@@ -186,7 +186,7 @@ export default function BinaryGame() {
                 className="px-4 py-2 cursor-pointer rounded-xl border hover:bg-gray-50"
               >
                 New Number
-              </button>
+              </button>     
               <button
                 onClick={() => setMessage(`ℹ️ Answer: ${toBinary(target)} is the binary for ${target}.`)}
                 className="px-4 py-2 cursor-pointer rounded-xl border hover:bg-gray-50"
